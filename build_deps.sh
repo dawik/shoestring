fb7b8599d78d01bcb53c6dbcbcefc402a709de79
David Wikmans <dwikmans@planview.com>
2017-09-16 02:55:17 +0200

    Fix bullet linking

--- build_deps.sh
+++ build_deps.sh
@@ -6,13 +6,13 @@ git submodule update --init --recursive
 cd bullet3/build3
 ./premake4_linux64 gmake
 cd ..
-#sed -i 's/\s\+\(printf("unknown chunk\\n")\);/\/\/\1/' */*/*/*.cpp
+sed -i 's/\s\+\(printf("unknown chunk\\n")\);/\/\/\1/' */*/*/*.cpp
 cd build3/gmake
 make
 cd ../../bin
-for i in *
+for i in *.a
 do
-  mv -- "$i" "${i/_*/}"
+  mv -- "$i" "${i/_*/\.a}"
 done
 
 cd ../../assimp
